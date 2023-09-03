d32f018b0d99cf976bebc5caa110e25a4a5f7b71
Shajjadkhan <shajjadkhan.me@gmail.com>
2023-09-03 23:35:34 +0600

    complete

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@ profileIcon.onclick = () =>{
 }
 const chatIcons = document.querySelector('.chatIcons');
 chatIcons.onclick = ()=>{
-  document.querySelector('.chatBox').classList.toggle('activeChatBox');
+  document.querySelector('.chatListContainerJs').classList.toggle('active-chat');
   
 }
 
 document.querySelector('.arrowIcon').onclick= ()=>{
   document.querySelector('.chatBox').classList.remove('activeChatBox');
+
+}
+document.querySelector('.bellIcon').onclick= ()=>{
+  document.querySelector('.notificationContainer').classList.toggle('active-notification');
+
+}
+document.querySelector('.userInput').onclick= ()=>{
+  document.querySelector('.postPopUpContainer').style.display="flex";
+
+}
+document.querySelector('.cancelBtn').onclick= ()=>{
+  document.querySelector('.postPopUpContainer').style.display="none";
 
 }
